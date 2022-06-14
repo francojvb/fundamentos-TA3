1246c7f7983bcf9b69fa2bc147e44852bfeef815
francojvb <vibrafranco@hotmail.com>
2022-06-13 21:14:48 -0500

    Zombie Amarillo

--- PapuEngine/Zombie.cpp
+++ PapuEngine/Zombie.cpp
@@ -5,7 +5,7 @@
 void Zombie::init(glm::vec2 position)
 {
 	_position = position;
-	_color.set(0, 255, 0, 255);
+	_color.set(255, 255, 0, 255);
 }
 
 void Zombie::update(const std::vector<std::string>& levelData)
